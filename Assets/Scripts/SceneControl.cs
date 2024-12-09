4c95997221f64d4bf9840025a5fe21a7f1684491
HORIZON-Donut <kiettisak.k2546@gmail.com>
2024-12-10 03:38:33 +0700

    Create and add scene control to canvas

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -1,29 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneControl : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    public static SceneControl instance;
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-    public void Nextlevel()
-    {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    public void Loadscene(string sceneName)
-    {
-        SceneManager.LoadSceneAsync(sceneName);
-    }
-
+	public void OnPlayButton()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void OnQuitButton()
+	{
+		Application.Quit();
+	}
 }
-
