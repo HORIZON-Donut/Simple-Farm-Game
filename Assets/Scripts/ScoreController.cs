faae13a7aced92089c8b4dd9e685826cec9b0e76
HORIZON-Donut <kiettisak.k2546@gmail.com>
2024-12-11 08:25:00 +0700

    Modified in game currency

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -5,22 +5,16 @@ using UnityEngine.UI;
 public class ScoreController : MonoBehaviour
 {
 	public int money = 100;
-	public GameObject MyScore;
-	TextMeshProUGUI MyScore_text;
+	public Text MyScore_text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        MyScore_text = MyScore.GetComponent<TextMeshProUGUI>();
-    }
-
-	void Update()
+	void Start()
 	{
 		MyScore_text.text = "$"+money;
 	}
-
-    // Update is called once per frame
+	 // Update is called once per frame
     public void AddScore(int amount)
     {
         money+=amount;
+		MyScore_text.text = "$"+money;
     }
 }
