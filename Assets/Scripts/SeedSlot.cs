4fa865f35683307cee77b6730775e9f42cd33523
HORIZON-Donut <kiettisak.k2546@gmail.com>
2024-12-11 09:32:34 +0700

    Add Seed slot script

---
+++ Assets/Scripts/SeedSlot.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SeedSlot : MonoBehaviour
+{
+	public Text amountTxt;
+	public Text levelTxt;
+	public Sprite icon;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
