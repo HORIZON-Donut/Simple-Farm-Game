a67e5910f268eef3a76709c049b5c8511d21471d
HORIZON-Donut <kiettisak.k2546@gmail.com>
2024-12-11 08:10:13 +0700

    Re-locate tools slot position

--- Assets/Scripts/Tools.cs
+++ Assets/Scripts/Tools.cs
@@ -5,8 +5,8 @@ public class Equipment : MonoBehaviour
 {
 	public  EquipmentObject tool;
 
-	public Text nameTxt;
 	public Image icon;
+	public Text NameTxt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +15,7 @@ public class Equipment : MonoBehaviour
     }
 	void initializationUI()
 	{
-		nameTxt.text = tool.toolsName;
+		NameTxt.text = tool.toolsName;
 		icon.sprite = tool.icon;
 	}
 
