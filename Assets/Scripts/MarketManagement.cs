45aed8844cd1f171496e1a59cf88953c3eeb3dcb
HORIZON-Donut <kiettisak.k2546@gmail.com>
2024-12-15 06:11:03 +0700

    Sort plant object done

--- Assets/Scripts/MarketManagement.cs
+++ Assets/Scripts/MarketManagement.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StoreManagement : MonoBehaviour
 {
 	public GameObject plantItem;
+	List<PlantObject> plantObjects = new List<PlantObject>();
 
 	private void Awake()
 	{
@@ -10,8 +12,21 @@ public class StoreManagement : MonoBehaviour
 
 		foreach(var plant in loadPlant)
 		{
+			plantObjects.Add((PlantObject)plant);
+		}
+		plantObjects.Sort(SortByPrice);
+
+		foreach(var plant in loadPlant)
+		{
 			PlantItem newPlant = Instantiate(plantItem, transform).GetComponent<PlantItem>();
 			newPlant.plant = (PlantObject)plant;
 		}
+
 	}
+
+	int SortByPrice(PlantObject plantObject1, PlantObject plantObject2)
+	{
+		return plantObject1.buyprice.CompareTo(plantObject2.buyprice);
+	}
+
 }
